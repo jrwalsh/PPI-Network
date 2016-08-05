bcc0267bff1848d626eea75180b775a790cf16c3
Jesse Walsh <jrwalsh@iastate.edu>
2016-08-05 10:45:03 -0500

    Update generateInteractions
    Modified the generateInteractions script to play nice with the new
    structure in this repo.  Hard-coded input files (for now) and modified
    the rest of the script to input from the new file location variables and
    output to the base results folder.

--- PPI-Network/Perl/generateInteractions.pl
+++ PPI-Network/Perl/generateInteractions.pl
@@ -31,38 +31,52 @@ if ($#ARGV < 0) {
 	exit;
 }
 
-my $pfamPredictions = $ARGV[0];
-my $ipfam = $ARGV[1];
-my $org = $ARGV[2];
+#my $pfamPredictions = $ARGV[0];
+#my $ipfam = $ARGV[1];
+#my $org = $ARGV[2];
+my $pfamPredictions = "../Data/Arabidopsis/pfam_predictions.tab";
+my $ipfam = "../Data/iPfam/iPfam_domain_interactions.tab";
+my $org = "Ara";
+my $resultsDir = "../Results";
 
-print "Using $pfamPredictions as pfamPredictions\n" if -p $pfamPredictions;
-print "Using $ipfam as ipfam interactions list\n" if -p $ipfam;
+if (-e $pfamPredictions) {
+	print "Using $pfamPredictions as pfamPredictions\n";
+} else {
+	print "Missing $pfamPredictions as pfamPredictions\n";
+	exit;
+}
+if (-e $ipfam) {
+	print "Using $ipfam as ipfam interactions list\n";
+} else {
+	print "Missing $ipfam as ipfam interactions list\n";
+	exit;
+}
 
 # Sort both the pfamPredictions list and the ipfam interactions list
 print "Sorting the pfamPredictions and ipfam interaction lists\n";
-`(head -n 1 pfam_predictions.tab && tail -n +2 pfam_predictions.tab | sort) > pfam_predictions.tab.sorted`;
-`(head -n 1 iPfam_domain_interactions.tab && tail -n +2 iPfam_domain_interactions.tab | sort) > iPfam_domain_interactions.tab.sorted`;
+`(head -n 1 $pfamPredictions && tail -n +2 $pfamPredictions | sort) > $resultsDir/pfam_predictions.tab.sorted`;
+`(head -n 1 $ipfam && tail -n +2 $ipfam | sort) > $resultsDir/iPfam_domain_interactions.tab.sorted`;
 
 # Join the predictions with the ipfam interactions to generate a list of domain-domain interactions
 print "Generating a domain-domain interaction list\n";
-`join --header -1 1 -2 1 pfam_predictions.tab.sorted iPfam_domain_interactions.tab.sorted > domain_interactions`;
+`join --header -1 1 -2 1 $resultsDir/pfam_predictions.tab.sorted $resultsDir/iPfam_domain_interactions.tab.sorted > $resultsDir/domain_interactions`;
 
 # Sort the domain-domain interactions
-`(head -n 1 domain_interactions && tail -n +2 domain_interactions | sort -k 6) > domain_interactions.sorted`;
+`(head -n 1 $resultsDir/domain_interactions && tail -n +2 $resultsDir/domain_interactions | sort -k 6) > $resultsDir/domain_interactions.sorted`;
 
 # Join the partner domain back to the proteins they were predicted on to generate a protein-protein network
 print "Generating a protein-protein interaction list\n";
-`join --header -1 6 -2 1 domain_interactions.sorted pfam_predictions.tab.sorted  > protein_interactions.tmp`;
+`join --header -1 6 -2 1 $resultsDir/domain_interactions.sorted $resultsDir/pfam_predictions.tab.sorted  > $resultsDir/protein_interactions.tmp`;
 
 # Remove unused columns in the protein-protein network
-`cut --delimiter=" " -f3,8 protein_interactions.tmp > protein_interactions.tmp2`;
+`cut --delimiter=" " -f3,8 $resultsDir/protein_interactions.tmp > $resultsDir/protein_interactions.tmp2`;
 
 # Remove duplication that occurs when the same domain-domain interaction occurs between two proteins
-`tail -n +2 protein_interactions.tmp2 | sort | uniq > protein_interactions.tmp3`;
+`tail -n +2 $resultsDir/protein_interactions.tmp2 | sort | uniq > $resultsDir/protein_interactions.tmp3`;
 
 # Remove reciprical links between proteins
-my $inFile = "./protein_interactions.tmp3";
-my $outFile = "./protein_interactions.tmp4";
+my $inFile = "$resultsDir/protein_interactions.tmp3";
+my $outFile = "$resultsDir/protein_interactions.tmp4";
 open (my $in, '<', $inFile) or die "could not open infile";
 open (my $out, '>', $outFile) or die "could not open outfile";
 
@@ -78,35 +92,35 @@ while (my $line = <$in>) {
 close $in;
 close $out;
 
-`sort protein_interactions.tmp4 | uniq > protein_interactions`;
+`sort $resultsDir/protein_interactions.tmp4 | uniq > $resultsDir/protein_interactions`;
 
 # Depending on the organism, remove the protein identifiers, leaving only gene-gene interactions
 print "Generating a gene-gene interaction list\n";
 if ($org eq "Maize") {
-	`sed -e 's/_P[0-9][0-9]//g' -e 's/_FGP[0-9][0-9][0-9]//g' protein_interactions > gene_interactions.tmp`;
+	`sed -e 's/_P[0-9][0-9]//g' -e 's/_FGP[0-9][0-9][0-9]//g' $resultsDir/protein_interactions > $resultsDir/gene_interactions.tmp`;
 } elsif ($org eq "Ara") {
 	print "Using organism Ara\n";
-	`sed -e 's/\\.[0-9][0-9]\\?//g' protein_interactions > gene_interactions.tmp`;
+	`sed -e 's/\\.[0-9][0-9]\\?//g' $resultsDir/protein_interactions > $resultsDir/gene_interactions.tmp`;
 } else {
 	print "Unknown organism, I don't know how to merge proteins-protein interactions into gene-gene interactions\n";
 	exit;
 }
 
 # Remove duplication that occurs when multiple protein-protein interactions exist between 2 genes
-`sort gene_interactions.tmp | uniq > gene_interactions`;
+`sort $resultsDir/gene_interactions.tmp | uniq > $resultsDir/gene_interactions`;
 
 # Cleanup unused files
 print "Cleaning temporary files\n";
-`rm pfam_predictions.tab.sorted`;
-`rm iPfam_domain_interactions.tab.sorted`;
-#`rm domain_interactions`;
-`rm domain_interactions.sorted`;
-`rm protein_interactions.tmp`;
-`rm protein_interactions.tmp2`;
-`rm protein_interactions.tmp3`;
-`rm protein_interactions.tmp4`;
-#`rm protein_interactions`;
-`rm gene_interactions.tmp`;
-#`rm gene_interactions`;
+`rm $resultsDir/pfam_predictions.tab.sorted`;
+`rm $resultsDir/iPfam_domain_interactions.tab.sorted`;
+#`rm $resultsDir/domain_interactions`;
+`rm $resultsDir/domain_interactions.sorted`;
+`rm $resultsDir/protein_interactions.tmp`;
+`rm $resultsDir/protein_interactions.tmp2`;
+`rm $resultsDir/protein_interactions.tmp3`;
+`rm $resultsDir/protein_interactions.tmp4`;
+#`rm $resultsDir/protein_interactions`;
+`rm $resultsDir/gene_interactions.tmp`;
+#`rm $resultsDir/gene_interactions`;
 
 print "Done!\n";
