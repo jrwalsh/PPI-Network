d761c31906e8ff58799fa4911a32786eccdb280c
Jesse Walsh <jrwalsh@iastate.edu>
2016-08-10 11:48:29 -0500

    Minor
    Beautify code a bit.  Make file executable.

--- PPI-Network/Perl/sortRowsInPlace.pl
+++ PPI-Network/Perl/sortRowsInPlace.pl
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 
 while (my $line = <>) {
-        chomp $line;
-        my @fields = split(" ",$line);
-        if ($fields[0] lt $fields[1]) {
+	chomp $line;
+	my @fields = split(" ",$line);
+	if ($fields[0] lt $fields[1]) {
 		print "$fields[0]\t$fields[1]\n";
 	} else {
 		print "$fields[1]\t$fields[0]\n";
