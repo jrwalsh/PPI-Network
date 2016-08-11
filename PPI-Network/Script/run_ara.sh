6e30df113bf9db042bb07d87f558fbf658bb7404
Jesse Walsh <jrwalsh@iastate.edu>
2016-08-11 13:14:17 -0500

    Automatically process pfam_scan output
    Now automatically clean and process the pfam_scan output to match the
    format needed by the generateInteractions script.  Previously this step
    was done by hand in excel.  The new version preserves domain locations.

--- PPI-Network/Script/run_ara.sh
+++ PPI-Network/Script/run_ara.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+# @Author Jesse R. Walsh 8/11/2016
+
 cd ~/Desktop/PPI\ Network/PFam/PfamScan/
 ./pfam_scan.pl \
 	-out ~/Desktop/PPI\ Network/outfile_ara.results \
 	-fasta ~/Desktop/PPI\ Network/Gene\ Model\ Sets/Ara/Araport11_genes.201606.pep.fasta \
 	-dir ~/Desktop/PPI\ Network/PFam/PfamScanDataFiles 
+
+less outfile_ara.results
+tail -n +29 outfile_ara.results | tr -s ' ' | cut --d ' ' -f1,2,3,6,8,12,13 > pfam_predictions.tab
+
+
+# Convert output to the following columns,  first print column headers
+echo -e 'pred_domain\tgene\ttype\tscore\teval\tstart\tend' > pfam_predictions.tab
+
+# Ignore the pfam_scan headers, condense spaces, cut only desired columns to keep, remove decimals in pfam domain names, rearrange columns and use tabs, and append to new file
+tail -n +29 outfile_ara.results | tr -s ' ' | cut --d ' ' -f1,2,3,6,8,12,13 | awk '{gsub(/\..*$/,"",$4)}1'| awk -v OFS='\t' '{print $4, $1, $5, $6, $7, $2, $3}' >> pfam_predictions.tab
